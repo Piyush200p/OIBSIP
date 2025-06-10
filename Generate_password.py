555d32abfeca85cd7427c8fc522dda5ae11618dc
piyu200p <bt24csa031@iiitn.ac.in>
2025-06-10 23:50:45 +0530

    advanced generate password

--- Generate_password.py
+++ Generate_password.py
@@ -1,17 +1,58 @@
+import tkinter as tk
+from tkinter import messagebox
 import random
 import string
-character=input("Enter (y/n) if you wwant to use character :")
-digit=input("Enter (y/n) if you wwant to use digit :")
-symbole=input("Enter (y/n) if you wwant to use symbole :")
-k = int(input("Enter the length of password :"))
-pp=''
-if(character=="y"):
-    pp+=string.ascii_letters
-if(digit=="y"):
-    pp+=string.digits
-if(symbole=="y"):
-    pp+=string.punctuation
-if(pp==""):
-    print("No Choice selected")
-password="".join(random.choices(pp, k=k ))
-print("Generated Password :",password)
+def generate_password():
+    character = char_var.get()
+    digit = digit_var.get()
+    symbole = sym_var.get()
+    try:
+        k  = int(length_var.get())
+    except ValueError:
+        result_var.set("Invalid length")
+        return
+    pp=''
+    if(character=="y"):
+        pp+=string.ascii_letters
+    if(digit=="y"):
+        pp+=string.digits
+    if(symbole=="y"):
+        pp+=string.punctuation
+    if(pp==""):
+        print("No Choice selected")
+        return
+    password="".join(random.choices(pp, k=k ))
+    result_var.set(password)
+def copy_to_clipboard():
+    password = result_var.get()
+    if password:
+        root.clipboard_clear()
+        root.clipboard_append(password)
+        messagebox.showinfo("Success", "Password copied to clipboard!")
+    else:
+        messagebox.showwarning("Warning", "No password to copy!")
+# gui setup
+root = tk.Tk()
+root.title("Password Generator")
+root.geometry("350x300")
+root.resizable(False, False)
+
+char_var = tk.StringVar()
+digit_var = tk.StringVar()
+sym_var = tk.StringVar()
+result_var = tk.StringVar()
+length_var = tk.StringVar()
+
+tk.Label(root, text="Enter (y/n) if you want to use characters:").pack()
+tk.Entry(root, textvariable=char_var).pack()
+tk.Label(root, text="Enter (y/n) if you want to use digits:").pack()
+tk.Entry(root, textvariable=digit_var).pack()
+tk.Label(root, text="Enter (y/n) if you want to use symbols:").pack()
+tk.Entry(root, textvariable=sym_var).pack()
+tk.Label(root, text= "Enter the length of password:").pack()
+tk.Entry(root, textvariable=length_var).pack()
+tk.Button(root, text="Generate Password", command=generate_password).pack()
+tk.Label(root, text="Generated Password:").pack()
+tk.Label(root, textvariable=result_var).pack()
+tk.Button(root, text="Copy to Clipboard", command=copy_to_clipboard).pack()
+root.mainloop()
